f28ce5922d94da02a33d2662638189ac4a144b2f
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-17 14:43:43 +0600

    Update Details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./component/Home";
 import { Navbar } from "./component/include/Navbar";
+import ProductDetails from "./component/ProductDetails";
+import Products from "./component/Products";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:id" component={ProductDetails} />
+      </Switch>
     </>
   );
 }
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
+import { NavLink } from "react-router-dom";
+import ProductSkeleton from "./Skeleton/ProductSkeleton";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -29,25 +31,6 @@ const Products = () => {
   const filterProduct = (cat) => {
     const updateList = data.filter((x) => x.category === cat);
     setFilter(updateList);
-  };
-
-  const Loading = () => {
-    return (
-      <>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-        <div className="col-md-3">
-          <Skeleton height={350} />
-        </div>
-      </>
-    );
   };
 
   const ShowProducts = () => {
@@ -103,9 +86,9 @@ const Products = () => {
                       {product.title.substring(0, 20)}...
                     </h5>
                     <p class="card-text lead fw-bold">${product.price}</p>
-                    <a href="#" className="btn btn-outline-dark">
+                    <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">
                       Buy Now
-                    </a>
+                    </NavLink>
                   </div>
                 </div>
               </div>
@@ -118,7 +101,7 @@ const Products = () => {
 
   return (
     <>
-      <div className="container my-5 py-5">
+      <div className="container my-5 ">
         <div className="row">
           <div className="col-12 mb-5">
             <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
@@ -126,7 +109,8 @@ const Products = () => {
           </div>
         </div>
         <div className="row justify-content-center">
-          {loading ? <Loading /> : <ShowProducts />}
+          <ProductSkeleton />
+          {loading ? <ProductSkeleton /> : <ShowProducts />}
         </div>
       </div>
     </>
