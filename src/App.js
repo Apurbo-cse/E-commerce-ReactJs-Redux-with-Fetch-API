857d0d2b0b60f50f72e610e562833fe9da12758a
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-17 11:26:53 +0600

    Product | Fakeapi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./component/Home";
 import { Navbar } from "./component/include/Navbar";
+import Products from "./component/Products";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Products />
     </>
   );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <>
       <div className="hero">
-        <div class="card text-bg-dark">
-          <img src="/assets/bg.png" class="card-img" alt="/" height={500} />
-          <div class="card-img-overlay d-flex flex-column justify-content-around">
+        <div className="card text-bg-dark">
+          <img src="/assets/bg.png" className="card-img" alt="/" height={500} />
+          <div className="card-img-overlay d-flex flex-column justify-content-around">
             <div className="container">
-              <h5 class="card-title display-3 text-mute fw-bolder mb-0">
+              <h5 className="card-title display-3 text-mute fw-bolder mb-0">
                 NEW SESSION ARRIVALS
               </h5>
-              <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+              <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
             </div>
           </div>
         </div>
